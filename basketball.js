1bd9c61176faaf66b392490e538e0314cc88da84
inomajon <amonijon6@aol.com>
2015-12-10 22:14:45 +0000

    added files

--- basketball.js
+++ basketball.js
@@ -1,3 +1,25 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
+    if(name.length>15){
+    alert("Wow, that's a long name");}
+    return name;
 };
+var tryTwoPointShot = function(){
+    var ran1 = Math.random();
+    var ran2 = Math.random();
+    if(ran1>0.2 && ran2>0.2){
+        return true
+    }else{ 
+        return false
+}; 
+var tryThreePointShot = function(){
+    var ran1 = Math.random();
+    var ran2 = Math.random();
+    if(ran1<=0.2 || ran2>=0.8){
+        return true
+    }else{
+        return false
+    }
+};
+var getShotString = function (playerName,points,wasSuccessful) {
+    return "attempted a pointer. It was (GOOD/ NO GOOD)"
