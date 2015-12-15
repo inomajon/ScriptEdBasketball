75a4c190f8e5076dcb403f23ce22f178df32b2f7
inomajon <amonijon6@aol.com>
2015-12-15 22:13:39 +0000

    Finish Game

--- basketball.js
+++ basketball.js
@@ -1,25 +1,49 @@
 var promptForPlayerName = function (playerNumber) {
     var name = prompt(playerNumber + " what is your name?");
-    if(name.length>15){
-    alert("Wow, that's a long name");}
+    if (name.length>15) {
+    alert("Wow, that's a long name");
+    }
     return name;
 };
 var tryTwoPointShot = function(){
     var ran1 = Math.random();
     var ran2 = Math.random();
-    if(ran1>0.2 && ran2>0.2){
-        return true
-    }else{ 
-        return false
+    if (ran1>0.2 && ran2>0.2) {
+        return true;
+    } else { 
+        return false;
+    }
 }; 
 var tryThreePointShot = function(){
     var ran1 = Math.random();
     var ran2 = Math.random();
-    if(ran1<=0.2 || ran2>=0.8){
-        return true
-    }else{
-        return false
+    if (ran1<=0.2 || ran2>=0.8) {
+        return true;
+    } else {
+        return false;
     }
 };
 var getShotString = function (playerName,points,wasSuccessful) {
-    return "attempted a pointer. It was (GOOD/ NO GOOD)"
+    var string= playerName + "attempted a " + points + " pointer";
+    if (wasSuccessful === true) {
+        return string + "it was good";
+    } else {
+        return string + "no good";
+    }
+};
+
+var updateScore = function(wasShotMade,currentScore,points){
+     if (wasShotMade === true){
+       currentScore = currentScore + points;
+       }
+       return currentScore
+}
+ var isEndOfGame = function(currentScore,playerName){
+     if (currentScore>=20){
+     alert ("The game is over " + playerName + " won");
+     return true;
+ } else {
+     return false;
+    }
+ }
+ 
